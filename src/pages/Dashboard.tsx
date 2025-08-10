ca8f4e39460e78a7d79bbc5f880f0f6f8de75b30
[dyad] <git@dyad.sh>
2025-08-10 13:38:06 +0200

    [dyad] Logo wird nun als Bild angezeigt - wrote 1 file(s)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import { PlatformChart } from "@/components/dashboard/PlatformChart";
 import { NicheChart } from "@/components/dashboard/NicheChart";
 import { ActivityHeatmap } from "@/components/dashboard/ActivityHeatmap";
 import { mockData } from "@/lib/mockData";
-import SafyLogo from '../assets/safy-logo.svg?react';
+import safyLogoUrl from '../assets/safy-logo.svg';
 
 const formatNumber = (num: number) => {
     if (num >= 1000) {
@@ -80,7 +80,7 @@ const DashboardPage = () => {
             </div>
         </div>
         <footer className="pt-10 pb-4 flex justify-center items-center">
-            <SafyLogo className="w-[150px]" />
+            <img src={safyLogoUrl} alt="SAFY Logo" className="w-[150px]" />
         </footer>
     </div>
   );
