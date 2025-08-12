bd9379fd67d3073ebb5d6180f8680b771cc49265
0xVolta-max <bjoern.ritzmann@gmail.com>
2025-08-12 15:30:45 +0200

    [dyad] Button-Position im Widget angepasst - wrote 1 file(s)

--- src/components/dashboard/TogglableWidget.tsx
+++ src/components/dashboard/TogglableWidget.tsx
@@ -27,7 +27,7 @@ const TogglableWidget = () => {
         </div>
       </div>
 
-      <div className="absolute top-2 left-2 z-20">
+      <div className="absolute top-2 right-2 z-20">
         <Button
           size="icon"
           variant="ghost"
