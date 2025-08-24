10ee05916531926ddafc268aac1a6679145a7635
0xVolta-max <bjoern.ritzmann@gmail.com>
2025-08-24 10:22:38 +0200

    fix: attempt to fix altcha widget type errors

--- src/components/AltchaWidget.tsx
+++ src/components/AltchaWidget.tsx
@@ -1,17 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import 'altcha';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'altcha-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        challengeurl?: string;
-        auto?: string;
-      };
-    }
-  }
-}
-
 interface AltchaWidgetProps {
   onVerified: (token: string) => void;
   onError: (error: string) => void;
@@ -19,7 +8,7 @@ interface AltchaWidgetProps {
 }
 
 const AltchaWidget: React.FC<AltchaWidgetProps> = ({ onVerified, onError, auto }) => {
-  const ref = useRef<HTMLElement>(null);
+  const ref = useRef<any>(null);
 
   useEffect(() => {
     const { current } = ref;
@@ -39,7 +28,7 @@ const AltchaWidget: React.FC<AltchaWidgetProps> = ({ onVerified, onError, auto }
     <altcha-widget
       ref={ref}
       challengeurl={import.meta.env.VITE_ALTCHA_CHALLENGE_API_URL}
-      auto={auto ? 'on' : undefined}
+      auto={auto ? 'onload' : undefined}
     ></altcha-widget>
   );
 };
